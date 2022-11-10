144ea5cb0ffcb01dfea6534d34f35b56aa68af48
LeeviIlli <99171073+LeeviIlli@users.noreply.github.com>
2022-11-10 12:52:26 +0200

    Delete components directory

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-    return (
-        <div class="footer">
-        <p>Blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa blaa.</p>
-          </div>
-    );
-}
--- components/Login.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function Login() {
-    return (
-        <div class="col-6">
-            <h1>Login</h1>
-            <p>Blaa</p>
-        </div>
-    )
-}
